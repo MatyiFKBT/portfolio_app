4e7714dd2b70af29145292727c415a885201f1d6
MatyiFKBT <mmatyi1999@gmail.com>
2019-09-02 21:50:07 +0200

    Updated test

--- test/main_test.dart
+++ test/main_test.dart
@@ -11,19 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:portfolio_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(Portfolio());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('Matyi'), findsOneWidget);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that there's text on the screen
+    expect(find.text('Matyi King'), findsOneWidget);
   });
 }
